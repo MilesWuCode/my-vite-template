c7dc55deef4401eef1a928a64e405f603e7fd7e2
Miles Wu <slion0725@gmail.com>
2022-01-14 17:49:43 +0800

    upd

--- src/components/pieces/Menu.vue
+++ src/components/pieces/Menu.vue
@@ -1,62 +1,27 @@
 <template>
   <ul>
     <li class="mb-6">
-      <h5 class="mb-3 text-gray-900">Getting Started</h5>
+      <h5 class="mb-3 text-gray-900">Account</h5>
       <ul class="space-y-6 lg:space-y-2">
         <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
+          <router-link class="block pl-4 text-gray-900" to="/login">Login</router-link>
         </li>
         <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
+          <router-link class="block pl-4 text-gray-900" to="/register">Register</router-link>
         </li>
         <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
-        </li>
-        <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
-        </li>
-        <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
+          <router-link class="block pl-4 text-gray-900" to="/todo">Todo</router-link>
         </li>
       </ul>
     </li>
     <li class="mb-6">
-      <h5 class="mb-3 text-gray-900">Getting Started</h5>
+      <h5 class="mb-3 text-gray-900">Page</h5>
       <ul class="space-y-6 lg:space-y-2">
         <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
+          <router-link class="block pl-4 text-gray-900" to="/">Index</router-link>
         </li>
         <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
-        </li>
-        <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
-        </li>
-        <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
-        </li>
-        <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
-        </li>
-      </ul>
-    </li>
-    <li class="mb-6">
-      <h5 class="mb-3 text-gray-900">Getting Started</h5>
-      <ul class="space-y-6 lg:space-y-2">
-        <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
-        </li>
-        <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
-        </li>
-        <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
-        </li>
-        <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
-        </li>
-        <li>
-          <a class="block pl-4 text-gray-900" href="/docs/installation">Installation</a>
+          <router-link class="block pl-4 text-gray-900" to="/test">Test</router-link>
         </li>
       </ul>
     </li>
