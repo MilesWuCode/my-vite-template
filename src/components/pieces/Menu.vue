df3490e85f0da1b9473d78640ba0e860ed73216e
Miles Wu <slion0725@gmail.com>
2022-01-18 14:49:20 +0800

    upd

--- src/components/pieces/Menu.vue
+++ src/components/pieces/Menu.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="menu">
-    <li class="menu-title">User</li>
+    <li class="menu-title">Member</li>
     <li>
       <router-link to="/login">Login</router-link>
     </li>
@@ -8,7 +8,17 @@
       <router-link to="/register">Register</router-link>
     </li>
     <li>
+      <router-link to="/profile">Profile</router-link>
+    </li>
+    <li>
       <router-link to="/todo">Todo</router-link>
+    </li>
+    <li class="menu-title">GraphQL</li>
+    <li>
+      <router-link to="/news">News</router-link>
+    </li>
+    <li>
+      <router-link to="/post">Post</router-link>
     </li>
   </ul>
 </template>
