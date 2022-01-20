635c4f07a87f45b94a2f973bee0e081aefbdfe1c
Miles Wu <slion0725@gmail.com>
2022-01-20 17:04:23 +0800

    upd

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -8,20 +8,12 @@ export const useAuthStore = defineStore('auth', {
   state: ():User => ({
     user: null,
   }),
-  // state: ():User => {
-  //   return {
-  //     user: null
-  //   }
-  // },
   getters: {
     loggedIn: (state): boolean => state.user !== null,
   },
   actions: {
     setUser(user: object|null) {
       this.user = user
-    },
-    randomizeCounter() {
-      // this.counter = Math.round(100 * Math.random())
     },
   },
 })
